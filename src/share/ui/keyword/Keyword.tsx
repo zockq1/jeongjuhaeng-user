6341669807e8b2496e9d6cf20110f808e060954f
zockq1 <zockq1@gmail.com>
2024-04-07 17:40:59 +0900

    Feat: Keyword 컴포넌트 추가

---
+++ src/share/ui/keyword/Keyword.tsx
@@ -0,0 +1,123 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+
+import { RootState } from '@/store/store';
+
+import Icon from '../icon/Icon';
+
+interface KeywordCommentBoxProps {
+  children?: string;
+  comment?: string;
+  file?: string;
+  commentOff?: boolean;
+}
+
+export default function Keyword({
+  children,
+  comment,
+  file,
+  commentOff = false,
+}: KeywordCommentBoxProps) {
+  const { isCommentOn: isCommentOnGlobal } = useSelector(
+    (state: RootState) => state.toggle,
+  );
+  const [isCommentOn, setIsCommentOn] = useState(
+    commentOff ? false : isCommentOnGlobal,
+  );
+
+  useEffect(() => {
+    if (commentOff) return;
+    setIsCommentOn(isCommentOnGlobal);
+  }, [isCommentOnGlobal, commentOff]);
+
+  const toggle = () => {
+    if (commentOff) return;
+    setIsCommentOn((prev) => !prev);
+  };
+
+  return (
+    <KeywordContainer onClick={toggle}>
+      <div>{children}</div>
+      {file && <Image src={file} />}
+      {isCommentOn && (
+        <CommentList>
+          {comment ? (
+            comment
+              .trim()
+              .split('.')
+              .filter(Boolean)
+              .map((item) => {
+                return (
+                  <Comment key={item}>
+                    <CommentIcon>
+                      <Icon icon="checkBox" size={12} />
+                    </CommentIcon>
+                    {item.trim()}
+                  </Comment>
+                );
+              })
+          ) : (
+            <Comment key="-">
+              <Icon icon="checkBox" size={12} /> &nbsp; -
+            </Comment>
+          )}
+        </CommentList>
+      )}
+    </KeywordContainer>
+  );
+}
+
+const KeywordContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: fit-content;
+  margin: 5px;
+  padding: ${({ theme }) => theme.padding.small};
+  border: ${({ theme }) => `1px solid ${theme.colors.lightGrey}`};
+  border-radius: 10px;
+  box-shadow: ${({ theme }) => theme.shadow.defaultShadow};
+
+  background-color: ${({ theme }) => theme.colors.keywordBg};
+
+  color: ${({ theme }) => theme.colors.textBlue};
+  font-weight: ${({ theme }) => theme.fontWeight.medium};
+  font-size: ${({ theme }) => theme.fontSizes.large};
+
+  cursor: pointer;
+
+  word-break: keep-all;
+`;
+
+const Image = styled.img`
+  max-width: 100px;
+  max-height: 100px;
+  margin: 10px 0 0;
+`;
+
+const CommentList = styled.ul`
+  margin-top: 8px;
+  border-top: ${({ theme }) => `1px solid ${theme.colors.lightGrey}`};
+`;
+
+const Comment = styled.li`
+  display: flex;
+
+  width: fit-content;
+  margin-top: 8px;
+
+  font-weight: ${({ theme }) => theme.fontWeight.regular};
+  font-size: ${({ theme }) => theme.fontSizes.small};
+  line-height: 120%;
+
+  word-break: keep-all;
+`;
+
+const CommentIcon = styled.div`
+  flex-shrink: 0;
+
+  width: 12px;
+  height: 12px;
+  margin-right: 4px;
+`;
