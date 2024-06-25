d9903ae90a85aaac351f2464119e37e9c7412eb6
zockq1 <zockq1@gmail.com>
2024-06-25 14:09:08 +0900

    Fix: 로그인 상태 변경시 캐시 무효화 하도록 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Outlet, useLocation } from 'react-router-dom';
 import { StyleSheetManager } from 'styled-components';
 
+import { chapterApi } from './store/api/chapterApi';
+import { jjhApi } from './store/api/jjhApi';
+import { questionApi } from './store/api/questionApi';
+import { timelineApi } from './store/api/timelineApi';
+import { topicApi } from './store/api/topicApi';
+import { RootState } from './store/store';
+
 export default function App() {
   const location = useLocation();
+  const dispatch = useDispatch();
+  const isMounted = useRef(false);
+  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location.pathname, location.search]);
+
+  useEffect(() => {
+    if (isMounted.current) {
+      dispatch(topicApi.util.resetApiState());
+      dispatch(chapterApi.util.resetApiState());
+      dispatch(timelineApi.util.resetApiState());
+      dispatch(jjhApi.util.resetApiState());
+      dispatch(questionApi.util.resetApiState());
+    } else {
+      isMounted.current = true;
+    }
+  }, [isLoggedIn, dispatch]);
 
   return (
     <main>
