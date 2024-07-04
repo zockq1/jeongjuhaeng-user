b95b5ba8d51174db2c370b3df2b66cb99aef6aaa
zockq1 <zockq1@gmail.com>
2024-07-04 19:54:55 +0900

    Refactor: 커스텀 훅으로 분리
    리프레시 로직, 스크롤 탑 로직

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
-import { useEffect, useRef } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { StyleSheetManager } from 'styled-components';
 
-import { jjhApi, useLazyGetTotalProgressQuery } from './store/api/jjhApi';
-import { RootState } from './store/store';
+import useCheckRefresh from './share/hook/useCheckRefresh';
+import usePrefetchSideMenu from './share/hook/usePrefetchSideMenu';
 
 export default function App() {
-  const [triger] = useLazyGetTotalProgressQuery();
-  const location = useLocation();
-  const dispatch = useDispatch();
-  const isMounted = useRef(false);
-  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [location]);
-
-  useEffect(() => {
-    if (!isMounted.current) {
-      isLoggedIn && triger().then();
-      dispatch(jjhApi.util.invalidateTags(['jjhUpdate']));
-    } else {
-      isMounted.current = true;
-    }
-  }, [isLoggedIn, dispatch, triger]);
+  usePrefetchSideMenu();
+  useCheckRefresh();
 
   return (
     <main>
