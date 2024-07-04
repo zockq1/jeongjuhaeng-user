5cfd0af45a759a4367d707238b8fe50732475ae6
zockq1 <zockq1@gmail.com>
2024-07-04 20:18:18 +0900

    Feat: prefetch에 사이드메뉴 추가

--- src/share/hook/usePrefetchSideMenu.ts
+++ src/share/hook/usePrefetchSideMenu.ts
@@ -1,17 +1,30 @@
 import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+import { usePrefetch } from '@/store/api/jjhApi';
+import { RootState } from '@/store/store';
 
 import { usePrefetch as usePrefetchChapter } from '../../store/api/chapterApi';
 import { usePrefetch as usePrefetchQuiz } from '../../store/api/questionApi';
 import { usePrefetch as usePrefetchTimeline } from '../../store/api/timelineApi';
 
 export default function usePrefetchSideMenu() {
+  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
   const prefetchChapter = usePrefetchChapter('getChapterList');
   const prefetchTiemline = usePrefetchTimeline('getTimelineList');
   const prefetchQuiz = usePrefetchQuiz('getQuestionCategoryList');
+  const prefetchJJH = usePrefetch('getJJHList');
 
   useEffect(() => {
     prefetchChapter();
     prefetchTiemline();
     prefetchQuiz();
-  }, [prefetchChapter, prefetchTiemline, prefetchQuiz]);
+    prefetchJJH(isLoggedIn);
+  }, [
+    prefetchChapter,
+    prefetchTiemline,
+    prefetchQuiz,
+    prefetchJJH,
+    isLoggedIn,
+  ]);
 }
