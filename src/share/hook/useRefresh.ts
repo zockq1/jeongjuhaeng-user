097746eba90e4a84be5e391231211d61f9fb8153
zockq1 <zockq1@gmail.com>
2024-07-05 18:04:20 +0900

    Feat: preconnect 설정

--- src/share/hook/useRefresh.ts
+++ src/share/hook/useRefresh.ts
@@ -6,23 +6,24 @@ import { RootState } from '@/store/store';
 
 export default function useRefresh() {
   const dispatch = useDispatch();
-  const { refreshToken, accessToken } = useSelector(
+  const { refreshToken, accessToken, isLoggedIn } = useSelector(
     (state: RootState) => state.auth,
   );
 
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_API_URL}refresh-token`, {
-      method: 'GET',
-      credentials: 'include',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: accessToken,
-        'Refresh-Token': refreshToken,
-      },
-    }).then((response) => {
-      dispatch(setRefreshToken(response.headers.get('Refresh-Token')));
-      dispatch(setAccessToken(response.headers.get('Authorization')));
-    });
-  }, [accessToken, refreshToken, dispatch]);
+    isLoggedIn &&
+      fetch(`${import.meta.env.VITE_API_URL}refresh-token`, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: accessToken,
+          'Refresh-Token': refreshToken,
+        },
+      }).then((response) => {
+        dispatch(setRefreshToken(response.headers.get('Refresh-Token')));
+        dispatch(setAccessToken(response.headers.get('Authorization')));
+      });
+  }, [accessToken, refreshToken, dispatch, isLoggedIn]);
   return;
 }
