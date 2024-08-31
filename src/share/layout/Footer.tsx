5bcd43d12b312bb44b169b70aa0135a81f6cf6bc
zockq1 <zockq1@gmail.com>
2024-08-31 13:30:30 +0900

    Fix: footer 시맨틱 태그 수정

--- src/share/layout/Footer.tsx
+++ src/share/layout/Footer.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import Logo from '@/share/ui/icon/Logo';
 import { media } from '@/theme/theme';
 
-const FooterContainer = styled.header`
+const FooterContainer = styled.footer`
   display: flex;
   flex-direction: column;
   align-items: center;
